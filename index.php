966fee453f5617a40ebf72afd6f31676c9424bcd
Roman <roma.lyashevich2002@gmail.com>
2022-02-24 11:53:09 +0500

    used-component-news.list

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?
 require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php');
 $APPLICATION->SetTitle("Главная");
-?>
-<div class="swiper">
+?><div class="swiper">
 	<div class="swiper-pagination">
 	</div>
 	<!-- Additional required wrapper -->
@@ -39,59 +38,114 @@ $APPLICATION->SetTitle("Главная");
 	<!-- If we need scrollbar -->
 	<!--    <div class="swiper-scrollbar"></div>-->
 </div>
-</div>
 <section class="privilege" id="about">
 	<h2 class="text-center"></h2>
 	<h2 class="text-center">С нами удобно и выгодно</h2>
-	<div data-aos="fade-down" class="card-container">
-		<div class="card-item">
-			<div class="item-headline">
-				01.
-				<h2>Надежный<br>
-					таксопарк</h2>
-			</div>
-			<div class="item-desc">
-				У нас дружный коллектив, прозрачные условия работы, новые машины для аренды оснащенные световыми коробами, видео-регистраторами, детскими креслами и газовым оборудованием
-			</div>
+	<!-- слайдер -->
+</section> <? $APPLICATION->IncludeComponent(
+				"bitrix:news.list",
+				"about_top_slider",
+				array(
+					"ACTIVE_DATE_FORMAT" => "d.m.Y",	// Формат показа даты
+					"ADD_SECTIONS_CHAIN" => "N",	// Включать раздел в цепочку навигации
+					"AJAX_MODE" => "N",	// Включить режим AJAX
+					"AJAX_OPTION_ADDITIONAL" => "",	// Дополнительный идентификатор
+					"AJAX_OPTION_HISTORY" => "N",	// Включить эмуляцию навигации браузера
+					"AJAX_OPTION_JUMP" => "N",	// Включить прокрутку к началу компонента
+					"AJAX_OPTION_STYLE" => "N",	// Включить подгрузку стилей
+					"CACHE_FILTER" => "N",	// Кешировать при установленном фильтре
+					"CACHE_GROUPS" => "N",	// Учитывать права доступа
+					"CACHE_TIME" => "36000000",	// Время кеширования (сек.)
+					"CACHE_TYPE" => "N",	// Тип кеширования
+					"CHECK_DATES" => "Y",	// Показывать только активные на данный момент элементы
+					"DETAIL_URL" => "",	// URL страницы детального просмотра (по умолчанию - из настроек инфоблока)
+					"DISPLAY_BOTTOM_PAGER" => "N",	// Выводить под списком
+					"DISPLAY_DATE" => "N",	// Выводить дату элемента
+					"DISPLAY_NAME" => "N",	// Выводить название элемента
+					"DISPLAY_PICTURE" => "Y",	// Выводить изображение для анонса
+					"DISPLAY_PREVIEW_TEXT" => "Y",	// Выводить текст анонса
+					"DISPLAY_TOP_PAGER" => "N",	// Выводить над списком
+					"FIELD_CODE" => array(	// Поля
+						0 => "PREVIEW_TEXT",
+						1 => "Название",
+						2 => "Описание для анонса",
+						3 => "Картинка для анонса",
+						4 => "Детальное описание",
+						5 => "",
+					),
+					"FILTER_NAME" => "",	// Фильтр
+					"HIDE_LINK_WHEN_NO_DETAIL" => "N",	// Скрывать ссылку, если нет детального описания
+					"IBLOCK_ID" => "1",	// Код информационного блока
+					"IBLOCK_TYPE" => "index",	// Тип информационного блока (используется только для проверки)
+					"INCLUDE_IBLOCK_INTO_CHAIN" => "N",	// Включать инфоблок в цепочку навигации
+					"INCLUDE_SUBSECTIONS" => "N",	// Показывать элементы подразделов раздела
+					"MESSAGE_404" => "",	// Сообщение для показа (по умолчанию из компонента)
+					"NEWS_COUNT" => "20",	// Количество новостей на странице
+					"PAGER_BASE_LINK_ENABLE" => "N",	// Включить обработку ссылок
+					"PAGER_DESC_NUMBERING" => "N",	// Использовать обратную навигацию
+					"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",	// Время кеширования страниц для обратной навигации
+					"PAGER_SHOW_ALL" => "N",	// Показывать ссылку "Все"
+					"PAGER_SHOW_ALWAYS" => "N",	// Выводить всегда
+					"PAGER_TEMPLATE" => "",	// Шаблон постраничной навигации
+					"PAGER_TITLE" => "Новости",	// Название категорий
+					"PARENT_SECTION" => "",	// ID раздела
+					"PARENT_SECTION_CODE" => "",	// Код раздела
+					"PREVIEW_TRUNCATE_LEN" => "",	// Максимальная длина анонса для вывода (только для типа текст)
+					"PROPERTY_CODE" => array(	// Свойства
+						0 => "url",
+						1 => "[url] Ссылка",
+						2 => "",
+					),
+					"SET_BROWSER_TITLE" => "N",	// Устанавливать заголовок окна браузера
+					"SET_LAST_MODIFIED" => "N",	// Устанавливать в заголовках ответа время модификации страницы
+					"SET_META_DESCRIPTION" => "N",	// Устанавливать описание страницы
+					"SET_META_KEYWORDS" => "N",	// Устанавливать ключевые слова страницы
+					"SET_STATUS_404" => "N",	// Устанавливать статус 404
+					"SET_TITLE" => "N",	// Устанавливать заголовок страницы
+					"SHOW_404" => "N",	// Показ специальной страницы
+					"SORT_BY1" => "SORT",	// Поле для первой сортировки новостей
+					"SORT_BY2" => "",	// Поле для второй сортировки новостей
+					"SORT_ORDER1" => "ASC",	// Направление для первой сортировки новостей
+					"SORT_ORDER2" => "",	// Направление для второй сортировки новостей
+					"STRICT_SECTION_CHECK" => "N",	// Строгая проверка раздела для показа списка
+				),
+				false
+			); ?><br>
+<!---->
+<div class="card-container card-container__noBg_reverse">
+	<div class="card-item">
+		<div data-aos="fade-right" class="item-headline">
+			02.
+			<h2>Прозрачные <br>
+				условия работы</h2>
 		</div>
-		<div class="card-image">
-			<img src="/local/templates/main/images/card-1.png" alt="">
+		<div data-aos="fade-left" class="item-desc">
+			<span>Прямая связь с директором таксопарка, новые машины для аренды, акции, розыгрыши ценных призов и бесплатных дней<br>
+				аренды</span>
 		</div>
 	</div>
-	<!---->
-	<div class="card-container card-container__noBg_reverse">
-		<div class="card-item">
-			<div data-aos="fade-right" class="item-headline">
-				02.
-				<h2>Прозрачные <br>
-					условия работы</h2>
-			</div>
-			<div data-aos="fade-left" class="item-desc">
-				<span>Прямая связь с директором таксопарка, новые машины для аренды, акции, розыгрыши ценных призов и бесплатных дней<br>
-					аренды</span>
-			</div>
+	<div data-aos="fade-right" class="card-image">
+		<img src="/local/templates/main/images/card-2.png" alt="">
+	</div>
+</div>
+<!---->
+<div data-aos="fade-up" class="card-container">
+	<div class="card-item__privilege">
+		<div class="item-headline">
+			03.
+			<h2 style="max-width: 24rem">Выгода от долгосрочного сотрудничества</h2>
 		</div>
-		<div data-aos="fade-right" class="card-image">
-			<img src="/local/templates/main/images/card-2.png" alt="">
+		<div class="item-desc">
+			<span>Специальные условия для водителей<br>
+				и поощрительные бонусы при долгосрочном сотрудничестве</span>
 		</div>
 	</div>
-	<!---->
-	<div data-aos="fade-up" class="card-container">
-		<div class="card-item__privilege">
-			<div class="item-headline">
-				03.
-				<h2 style="max-width: 24rem">Выгода от долгосрочного сотрудничества</h2>
-			</div>
-			<div class="item-desc">
-				<span>Специальные условия для водителей<br>
-					и поощрительные бонусы при долгосрочном сотрудничестве</span>
-			</div>
-		</div>
-		<div class="card-image">
-			<img src="/local/templates/main/images/card-3.png" alt="">
-		</div>
+	<div class="card-image">
+		<img src="/local/templates/main/images/card-3.png" alt="">
 	</div>
-	<!---->
+</div>
+
+<!---->
 </section>
 <section class="step" id="get_started">
 	<div class="container">
@@ -540,11 +594,12 @@ $APPLICATION->SetTitle("Главная");
 				</div>
 				<br>
 				<div>
-					<input class="form_btn" type="submit" name="submit_btn" value="Отправить заявку">
+					<input class="form_btn" type="submit" name="submit_btn" value="Отправить заявку"><br>
+					<br>
 				</div>
 			</form>
 		</div>
 	</div>
-</section> <br><?
-				require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php');
-				?>
+	<?
+	require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php');
+	?>
