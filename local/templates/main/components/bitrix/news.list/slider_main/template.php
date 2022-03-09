f1dd6b3bed803a72b3fdbeb7f07252e632411cd3
Roman <roma.lyashevich2002@gmail.com>
2022-03-09 16:11:23 +0500

    deleted-link-in-template.php

--- local/templates/main/components/bitrix/news.list/slider_main/template.php
+++ local/templates/main/components/bitrix/news.list/slider_main/template.php
@@ -27,12 +27,10 @@ $this->setFrameMode(true);
 						$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
 						$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
 						?>
-						<a href="<?= $arItem["DETAIL_PAGE_URL"] ?>">
-							<img src="<?= $arItem["PREVIEW_PICTURE"]["SRC"] ?>" alt="<?= $arItem["PREVIEW_PICTURE"]["ALT"] ?>" />
+						<img src="<?= $arItem["PREVIEW_PICTURE"]["SRC"] ?>" alt="<?= $arItem["PREVIEW_PICTURE"]["ALT"] ?>" />
 					</div>
+				<? endforeach; ?>
 			</div>
-		<? endforeach; ?>
 		</div>
-	</div>
 	</div>
 <? endif; ?>
